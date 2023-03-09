e6658964f105fa7a68bcadae4f2c2a8a10e76866
NorvicNorvic <navillavic@gmail.com>
2023-03-09 14:58:00 -0300

    Incorporacion de filtrado de eventos en past y upcoming events

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,39 @@
-
 const contenedorTarjetas = document.getElementById('imagenes')
 
-function fechaEventoPasado(arrayData, date){
-  
-  let fechaBase=data.currentDate
-  let basisDate=parseInt (fechaBase.split("-"))
-  let dateEvent=parseInt(date.split("-"))
+contenedorTarjetas.innerHTML = crearTarjetas(data.events)
 
-  if(dateEvent < basisDate){
-   return true
-   } else
-   return false
-}
+function crearTarjetas(arrayData) {
 
+  let cards = ''
 
-const eventPast = data.events.filter(evento => fechaEventoPasado(data, evento.date))
-
-contenedorTarjetas.innerHTML=crearTarjetas(eventPast)
-
-
-function crearTarjetas(arrayData){
-
- let cards = '' //defino un string vacío
-
- for (const evento of arrayData) {
-   cards += `<article> <div class="cards" style="width: 18rem;">
+  for (const evento of arrayData) {
+    cards += `<article> <div class="cards" style="width: 18rem;">
 
 <img class="card-img-top" src="${evento.image}" alt="">
 <h4>${evento.name}</h4>
 <div class="parrafo">
- <p>${evento.description}</p>
+  <p>${evento.description}</p>
 </div>
 
 <div class="bootonCard">
 <div class="precio">
- <p>Precio $${evento.price}</p>
+  <p>Precio $${evento.price}</p>
 </div>
 <div id="form">
-<form method="get" action="./details.html" >
-<a id="aDetails" href="./details.html?id=${evento.id}">
-  Read more...
-</a>
-</form>
+  <form method="get" action="./details.html" >
+    <a id="aDetails" href="./details.html?id=${evento.id}">
+      Read more...
+    </a>
+  </form>
 </div>
 </div>
 </article>`
- }
- return cards
+  }
+  return cards
 
 }
+
+
 
 // //BUSQUEDA POR INPUT
 const inputBus = document.getElementById('inputBus')
@@ -96,7 +81,7 @@ button.addEventListener('click', (evento) => {
   
  let categoryArray = Array.from(arrayInput).map(categ => categ.value)
 
-const checkFiltrados = eventPast.filter((evento) => evento.category.includes(categoryArray))
+const checkFiltrados = data.events.filter((evento) => evento.category.includes(categoryArray))
 
 contenedorTarjetas.innerHTML = crearTarjetas(checkFiltrados)
 
--- scripts/upcoming_events.js
+++ scripts/upcoming_events.js
@@ -1,78 +1,103 @@
 
+const contenedorTarjetas = document.getElementById('imagenes')
+
 function fechaEventoPasado(arrayData, date){
   
-    let fechaBase=data.currentDate
-    let basisDate=parseInt (fechaBase.split("-"))
-    let dateEvent=parseInt(date.split("-"))
- 
-    if(dateEvent >= basisDate){
-     return true
-     } else
-     return false
- }
- 
- 
- const eventPast = data.events.filter(evento => fechaEventoPasado(data, evento.date))
- 
- const contenedorTarjetas = document.getElementById('imagenes')
- 
- contenedorTarjetas.innerHTML=crearTarjetas(eventPast)
- 
- 
- function crearTarjetas(arrayData){
- 
-   let cards = '' //defino un string vacío
- 
-   for (const evento of arrayData) {
-     cards += `<article> <div class="cards" style="width: 18rem;">
- 
- <img class="card-img-top" src="${evento.image}" alt="">
- <h4>${evento.name}</h4>
- <div class="parrafo">
-   <p>${evento.description}</p>
- </div>
- 
- <div class="bootonCard">
- <div class="precio">
-   <p>Precio $${evento.price}</p>
- </div>
- <div id="form">
- <form method="get" action="./details.html" >
- <a id="aDetails" href="./details.html?id=${evento.id}">
-   Read more...
- </a>
+  let fechaBase=data.currentDate
+  let basisDate=parseInt (fechaBase.split("-"))
+  let dateEvent=parseInt(date.split("-"))
+
+  if(dateEvent >= basisDate){
+   return true
+   } else
+   return false
+}
+
+
+const eventUpcom = data.events.filter(evento => fechaEventoPasado(data, evento.date))
+
+contenedorTarjetas.innerHTML=crearTarjetas(eventUpcom)
+
+
+function crearTarjetas(arrayData){
+
+ let cards = '' //defino un string vacío
+
+ for (const evento of arrayData) {
+   cards += `<article> <div class="cards" style="width: 18rem;">
+
+<img class="card-img-top" src="${evento.image}" alt="">
+<h4>${evento.name}</h4>
+<div class="parrafo">
+ <p>${evento.description}</p>
+</div>
+
+<div class="bootonCard">
+<div class="precio">
+ <p>Precio $${evento.price}</p>
+</div>
+<div id="form">
+<form method="get" action="./details.html" >
+<a id="aDetails" href="./details.html?id=${evento.id}">
+  Read more...
+</a>
 </form>
- </div>
- </div>
- </article>`
-   }
-   return cards
- 
+</div>
+</div>
+</article>`
  }
- 
- 
- //CATEGORÍAS DINÁMICAS
-var checkboxes = document.getElementById('checkBox')
+ return cards
 
-let even= data.events
-let categoria =  even.map(categ => categ.category)
+}
 
-categoria = categoria.reduce((ant, curr)=> {
-  if(ant.findIndex((a) => a.toLowerCase() == curr.toLowerCase()) == -1){
-   ant.push(curr)
+// //BUSQUEDA POR INPUT
+const inputBus = document.getElementById('inputBus')
+
+inputBus.addEventListener('keyup', () => {
+  const eventosFiltrados = data.events.filter((evento) => evento.name.toLowerCase().includes(inputBus.value.toLowerCase())
+    || evento.description.toLowerCase().includes(inputBus.value.toLowerCase()))
+
+  contenedorTarjetas.innerHTML = crearTarjetas(eventosFiltrados)
+
+  if (eventosFiltrados.length === 0) {
+      alert("The event does not exist")
+  }
+})
+
+ 
+//CATEGORiAS DINAMICAS
+const checkboxes = document.getElementById('checkBox')
+
+let even = data.events
+let categoria = even.map(categ => categ.category)
+
+categoria = categoria.reduce((ant, curr) => {
+  if (ant.findIndex((a) => a.toLowerCase() == curr.toLowerCase()) == -1) 
+  {
+    ant.push(curr)
   }
   return ant
 }, [])
 
 
 const checkBox = categoria.map(categoria => `<li class="nav-item"> 
-<input class="form-check-input" name="checkbox" type="checkbox" id="inlineCheckbox1" value="option1">
-<label class="form-check-label" for="inlineCheckbox1">${categoria}</label>
- </li>`)
+  <input class="form-check-input input-check" name="checkbox" type="checkbox"  value=${categoria}>
+  <label class="form-check-label" for="inlineCheckbox">${categoria}</label></li>`)
+  
+  checkboxes.innerHTML = checkBox
 
- checkboxes.innerHTML=checkBox
- 
- 
- 
- 
- 
+//SELECCION DE CHECKBOX
+
+button.addEventListener('click', (evento) => {
+  evento.preventDefault()
+  const button = document.querySelector("#button")
+
+  let arrayInput = document.querySelectorAll('input:checked')
+  
+ let categoryArray = Array.from(arrayInput).map(categ => categ.value)
+
+const checkFiltrados = eventUpcom.filter((evento) => evento.category.includes(categoryArray))
+
+contenedorTarjetas.innerHTML = crearTarjetas(checkFiltrados)
+
+})
